e0d229e97d4bf848da9c6fcbb46457953d96c5dd
Dragon2000v <shmatko064@gmail.com>
2024-04-20 13:57:29 +0300

    init

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+// eslint-disable-next-line no-irregular-whitespace
+  plugins: [react()],
+// eslint-disable-next-line no-irregular-whitespace
+  build: {
+// eslint-disable-next-line no-irregular-whitespace
+    sourcemap: true,
+// eslint-disable-next-line no-irregular-whitespace
+  }
+});
